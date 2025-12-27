5161e00fca890c4ab079aa62faadd791f40315b0
Mohammad Shadman <41700092+Mohammad-Shadman@users.noreply.github.com>
2025-12-27 07:46:26 -0500

    Add additional encrypter.exe commands in test.sh

--- test.sh
+++ test.sh
@@ -9,4 +9,11 @@ rm keyAsABCDEncrypted.txt
 ./encrypter.exe main.c -i ABCD  keyAsABCDEncrypted.txt
 ./encrypter.exe keyAsABCDEncrypted.txt -i ABCD  keyAsABCD-Dencrypted.txt
 ./encrypter.exe keyFileEncrypted.txt -f key  keyFileDecrypted.txt
+./encrypter.exe out.txt -f key out.txt -r
+./encrypter.exe out.txt -f key out.txt -r
+./encrypter.exe out.txt -f key out.txt -q
+./encrypter.exe out.txt -f key out.txt
+rm out.txt
+./encrypter.exe out.txt -f key out.txt
+./encrypter.exe main.c -f key out.txt -r
 sleep 60
